dc9d87df7efd9ebeddd11c558ffcc703cf748064
SS <ss@SSs-MacBook-Air.local>
2019-06-03 05:09:21 +0300

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,12 +1,16 @@
 
 
-var cards = ["queen","queen","king","king"];
+var cards = [{rank:"queen",suit:"hearts",cardImage:"images/queen-of-hearts.png"},
+{rank:"queen",suit:"diamonds",cardImage:"images/queen-of-diamonds.png"},
+{rank:"king",suit:"hearts",cardImage:"images/king-of-hearts.png"},
+{rank:"king",suit:"diamonds",cardImage:"images/king-of-diamonds.png"}
+];
 var cardsInPlay=[];
 // function fipover
 function flipCard(cardId)
 {
-cardsInPlay.push( cards[cardId]);
-console.log("User flipped " + cards[cardId]);
+cardsInPlay.push( cards[cardId].rank);
+console.log("User flipped " + cards[cardId].rank+ ""+cards[cardId].cardImage);
 }
 
 function checkForMatch(){
@@ -16,7 +20,7 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("Sorry, try again.");
 }}
 flipCard(0);
-flipCard(1);
+flipCard(2);
 checkForMatch();
 
 
