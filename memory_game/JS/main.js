87f5cbe770be15a38b0630df2c30edafa5aa734d
SS <ss@SSs-MacBook-Air.local>
2019-06-03 03:44:35 +0300

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -2,7 +2,25 @@
 
 var cards = ["queen","queen","king","king"];
 var cardsInPlay=[];
-var cardOne=cards[0];
+// function fipover
+function flipCard(cardId)
+{
+cardsInPlay.push( cards[cardId]);
+console.log("User flipped " + cards[cardId]);
+}
+
+function checkForMatch(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}}
+flipCard(0);
+flipCard(1);
+checkForMatch();
+
+
+/*var cardOne=cards[0];
 cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
 var cardTwo=cards[2];
@@ -12,6 +30,6 @@ if(cardsInPlay.length===2)
 	{if(cardsInPlay[0]===cardsInPlay[2])
      alert("You found a match!");
     else
-   { alert("Sorry, try again.");}}
+   { alert("Sorry, try again.");}}*/
 
 
